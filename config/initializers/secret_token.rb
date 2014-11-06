1df13a3de9d8688eec19ebbfbe9608c7d39c0894
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-11-06 13:48:08 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notifier::Application.config.secret_key_base = 'd11443d817554e14b12b5680f03546a53af6b55f1db3264758a132bc14b4074959ba1fdf66f2acb6c9961c312008fdf6868cdf854d2491b1ddb6f2bf7037bfb0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Notifier::Application.config.session_store :cookie_store, key: '_notifier_session'
