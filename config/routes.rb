9a5c186e0c5aa995f661681f92c097477c1b3636
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-11-25 16:40:44 +0600

    Change Google+ auth to server mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Notifier::Application.routes.draw do
 
-  resources :users
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+
   root 'sites#index'
-
-  #match '/users/new', to: 'users#new', via: 'post'
-  match '/signup', to: 'users#login', via: 'get'
-
-  #match '/sites', to: 'sites#index', via: 'get'
-  #match '/sites/:user_id', to: 'sites#show', via: 'get'
-  #match '/sites', to: 'sites#create', via: 'post'
-  #match '/sites/:id', to: 'sites#destroy', via: 'delete'
 
   resources :sites, except: [:new, :edit]
 
