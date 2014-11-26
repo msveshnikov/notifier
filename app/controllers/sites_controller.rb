834c25af42817cc7d9b39803e18f22b9aa6b4e60
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-11-26 13:11:28 +0600

    validation of url

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -11,13 +11,16 @@ class SitesController < ApplicationController
     @site = current_user.sites.build
     url = params[:site][:url]
     url = "http://#{url}" unless url[0..3]==:http
-    @site.url=url
-    clnt = HTTPClient.new
-    @site.last_updated = clnt.head(url).header['Last-Modified'][0]
-    doc = Nokogiri::HTML(clnt.get_content(url))
-    ## TODO: validate
-    @site.hash_content = Digest::MD5.hexdigest(doc.xpath("//body").first)
-    @site.save!
+    @site.url = url
+    begin
+      clnt = HTTPClient.new
+      @site.last_updated = clnt.head(url).header['Last-Modified'][0]
+      doc = Nokogiri::HTML(clnt.get_content(url))
+      @site.hash_content = Digest::MD5.hexdigest(doc.xpath("//body").first)
+      @site.save!
+    rescue
+      flash[:error] = "Wrong URL!"
+    end
     redirect_to root_path
   end
 
@@ -27,9 +30,4 @@ class SitesController < ApplicationController
     redirect_to root_path
   end
 
-  private
-
-  def site_params
-    params.require(:site).permit(:url)
-  end
 end
