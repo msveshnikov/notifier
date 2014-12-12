0f8ebbb723e4737275f317ed96c7df0487f79a27
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-12 23:50:54 +0600

    facebook login

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 gem 'omniauth-google-oauth2'
+gem 'omniauth-facebook'
 
 # Use mysql as the database for Active Record
 gem 'sqlite3'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '803335827642-kkrjdmaqps0n139revsg01qsiipi8572.apps.googleusercontent.com',
            'R9lqNYjY5NCieSC-BCnObCoU',
            {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+
+  provider :facebook, '196393753869092', 'b0d6cb5b3910cbf7b744964773a96651',
+           {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
