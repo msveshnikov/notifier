ed5968ec77866ee6d32cdb77bac267c6986fa808
Max Sveshnikov <msveshnikov@thumbtack.net>
2015-04-03 13:28:05 +0600

    +x bit on rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,12 +33,11 @@ namespace :deploy do
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
+  after :restart, :x_bin_rails do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute :chmod, "u+x bin/rails"
+      end
     end
   end
 
